94f49c3c5808ef6269ff3e32390266f6a5c43fdd
Ilyxsss <ilyas.semgane@univ-tlse3>
2026-03-31 08:57:28 +0200

    FIN TP 2

---
+++ src/objet/Equipement.java
@@ -0,0 +1,15 @@
+package objet;
+
+public enum Equipement {
+	CASQUE("casque"), BOUCLIER("bouclier");
+
+	private String nom;
+
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
+	@Override
+	public String toString() {
+		return nom;
+	}
+}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,14 +1,18 @@
 package personnages;
 
+import objet.Equipement;
+
 public class Romain {
 	private String nom;
 	private int force;
-
+	private String texte;
+	private int nbEquipement = 0;
+	private Equipement[] equipements;
 
 	public static void main(String[] args) {
 		Romain Minus = new Romain("Minus", -6);
 	}
-	
+
 	private Boolean isInvariantVerified() {
 		return force >= 0;
 	}
@@ -17,6 +21,7 @@ public class Romain {
 		super();
 		this.nom = nom;
 		this.force = force;
+		assert IsInvariantVerified();
 	}
 
 	public String getNom() {
@@ -31,11 +36,19 @@ public class Romain {
 		return "Le Romain " + nom + " : ";
 	}
 
+	private boolean IsInvariantVerified() {
+		return force >= 0;
+	}
+
 	public String toString() {
 		return nom;
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		//précondition de la force du coup est positive
+		assert forceCoup > 0;
+		//post condition : la force a diminuée
+		int oldForce = force;
 		force = force - forceCoup;
 		if (force < 1) {
 			force = 0;
@@ -43,5 +56,34 @@ public class Romain {
 		} else {
 			parler("Aïe");
 		}
+		//post condition : la force a diminuée
+		assert force < oldForce;
+		
+		assert IsInvariantVerified();
 	}
+
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		case 1:
+			if (equipements[0].equals(equipement)) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement + ".");
+			} else {
+				equiperSoldat(equipement);
+			}
+			break;
+		default:
+			equiperSoldat(equipement);
+			break;
+		}
+	}
+
+	private void equiperSoldat(Equipement equipement) {
+		equipements[nbEquipement] = equipement;
+		nbEquipement++;
+		System.out.println("Le soldat " + nom + " s'équipe avec " + equipement);
+	}
+
 }
